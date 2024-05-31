85fbe6eba9200d1c1f73584891d09fa16874c52c
richard <26688060+rchrd-0@users.noreply.github.com>
2024-05-31 09:56:14 +0800

    theming: completion menus

--- init.lua
+++ init.lua
@@ -854,6 +854,15 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     lazy = false,
     opts = {
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+        }
+      end,
       dimInactive = true,
       colors = {
         theme = {
