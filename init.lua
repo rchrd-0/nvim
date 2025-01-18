f8f8ae7aa15ca9c6ab4df9d261bfc7a6e701834c
rchrd <26688060+rchrd-0@users.noreply.github.com>
2025-01-18 19:11:05 +0800

    wip

--- init.lua
+++ init.lua
@@ -13,8 +13,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -25,17 +23,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,9 +1,20 @@
 return {
+  { 'v', 'J', ":m '>+1<CR>gv=gv", { silent = true } },
+  { 'v', 'K', ":m '<-2<CR>gv=gv", { silent = true } },
+  { 'n', 'J', 'mzJ`z' },
+  { 'n', '<C-d>', '<C-d>zz' },
+  { 'n', '<C-u>', '<C-u>zz' },
+  { 'n', 'n', 'nzzzv' },
+  { 'n', 'N', 'Nzzzv' },
+  { 'n', 'G', 'Gzz' },
+
   { 'n', '<M-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true } },
   { 'n', '<M-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true } },
   { 'n', '<M-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true } },
   { 'n', '<M-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true } },
   { 'n', '<M-\\>', ':TmuxNavigatePrevious<CR>', { noremap = true, silent = true } },
+  { 'n', '<M-[>', ':buffer #<CR>', { desc = 'Switch to the alternate buffer', silent = true } },
+  { 'n', '<M-]>', ':buffer #<CR>', { desc = 'Switch to the alternate buffer', silent = true } },
 
   { 'n', '<Esc>', '<cmd>nohlsearch<CR>' },
   { 't', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' } },
