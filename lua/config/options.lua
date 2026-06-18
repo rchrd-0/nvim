5bf2dd8fc9a5c419be11f4dedb07c8f36b2d5e37
rchrd <26688060+rchrd-0@users.noreply.github.com>
2026-06-18 22:17:04 +0800

    update!: 260618; treesitter, lspconfig breaking lock bump

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.filetype.add({
+  extension = {
+    pac = "javascript",
+  },
+})
+
 vim.g.snacks_animate = false
 vim.g.lazyvim_picker = "auto"
 vim.g.ai_cmp = false
